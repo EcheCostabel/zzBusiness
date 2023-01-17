1aad3a6a34ff0971478f66d4f3995671c7d49a32
EcheCostabel <exequielcostabel@gmail.com>
2023-01-17 10:56:51 -0300

    Fix responsive

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,32 +1,31 @@
 import React from 'react'
 import img2 from './img2.jpeg'
-import { UilShoppingCartAlt } from '@iconscout/react-unicons'
 import './Slider.css'
 
 function Slider() {
   return (
     <div className='sliderContainer'>
       <img src={img2} alt='img' class='imgg' />
-      <div className='pt-5 pb-5 container1'>
-        <div className='col-xs-12 col-md-4 col-lg-4 div1'>
+      <div className='row pt-5 pb-5 container1 '>
+        <div className='col-sm-12 col-md-4 col-lg-4 div1'>
+          
           <div className='divIcons'>
             <ion-icon name="cart"></ion-icon>
             <p className='text-center divP'>Variedad de productos</p>
           </div>
         </div>
 
-        <div className='col-xs-12 col-md-4 col-lg-4 div1'>
+        <div className='col-sm-12 col-md-4 col-lg-4 div1'>
           <div className='divIcons'>
-          <ion-icon name="logo-usd"></ion-icon>
-          
-          <p className='text-center divP'>El mejor precio del mercado</p>
+            <ion-icon name="logo-usd"></ion-icon>
+            <p className='text-center divP'>El mejor precio del mercado</p>
           </div>
         </div>
 
-        <div className='col-xs-12 col-md-4 col-lg-4 div1'>
-        <div className='divIcons'>
-        <ion-icon name="call"></ion-icon>          
-          <p className='text-center divP'>Atención personalizada</p>
+        <div className='col-sm-12 col-md-4 col-lg-4 div1'>
+          <div className='divIcons'>
+            <ion-icon name="call"></ion-icon>
+            <p className='text-center divP'>Atención personalizada</p>
           </div>
         </div>
       </div>
